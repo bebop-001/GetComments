41cdaf51c246aac7f7bb0f683912652d10d64762
sjs <sjs@sonic.net>
2014-05-25 12:59:50 -0700

    prior to move of button method to fragment class file.

--- src/com/example/getcomments/CommentsActivity.java
+++ src/com/example/getcomments/CommentsActivity.java
@@ -1,19 +1,20 @@
 package com.example.getcomments;
 
-import android.app.Activity;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
-public class CommentsActivity extends Activity {
+public class CommentsActivity extends FragmentActivity {
 	private static final String tag = "CommentsActivity";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.comments_layout);
 		Log.i(tag, "onCreate");
+		setContentView(R.layout.comments_fragment);
 	}
 	public void CommentActivityButtons(View view) {
 		int buttonId = view.getId();
@@ -38,5 +39,38 @@ public class CommentsActivity extends Activity {
 		else {
 			Log.i(tag, "UNKOWN BUTTON:" + buttonId);
 		}
+	}
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+        Log.i(tag, "onConfigurationChanged");
+		super.onConfigurationChanged(newConfig);
+	}
+	@Override
+	protected void onDestroy() {
+        Log.i(tag, "onDestroy");
+        // For this activity, onDestroy typically means orientation 
+        // change.  Finish the activity.
+        finish();  
+		super.onDestroy();
+	}
+	@Override
+	protected void onPause() {
+        Log.i(tag, "onPause");
+		super.onPause();
+	}
+	@Override
+	protected void onResume() {
+        Log.i(tag, "onResume");
+		super.onResume();
+	}
+	@Override
+	protected void onStart() {
+        Log.i(tag, "onStart");
+		super.onStart();
+	}
+	@Override
+	protected void onStop() {
+        Log.i(tag, "onStop");
+		super.onStop();
 	}
 }
